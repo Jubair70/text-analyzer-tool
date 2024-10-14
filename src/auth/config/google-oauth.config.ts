ef48662f20d547ad805b91841702abeeae304ce9
Jubair70 <jubair.hossain@yahoo.com>
2024-10-14 11:33:45 +0600

    removed secret

--- src/auth/config/google-oauth.config.ts
+++ src/auth/config/google-oauth.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('googleOAuth', () => ({
-  clinetID: process.env.GOOGLE_CLIENT_ID || "539478373835-jsnsji4cmdu3pimj568rkg2q03gpdstb.apps.googleusercontent.com",
-  clientSecret: process.env.GOOGLE_SECRET || "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" ,
+  clinetID: process.env.GOOGLE_CLIENT_ID ,
+  clientSecret: process.env.GOOGLE_SECRET ,
   callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/api/v1/auth/google/callback",
 }));
